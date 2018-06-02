18063e13784cfd46d037c49f29ec6a68cfc58218
Eldad7 <eldad.corem@gmail.com>
2018-06-02 13:50:15 +0300

    Check if idol is on our system

--- getIdol.php
+++ getIdol.php
@@ -34,6 +34,27 @@
 											)
 										));
 			}
+			//Check if user exists on our DB - if yes - say when is the latests analysis
+			$url = 'https://api.mlab.com/api/1/databases/analysis/collections/idols';
+			$url.='?q={"_id":"'.$resultArray['data']['userID'].'"}';
+			$url.='&apiKey=tvG8BMjzxtNwm3fRgQv4LNbcF2IIeWWc&';
+			
+			//Get all idols where no data has been collected for yet
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		    'Content-Type: application/json',
+		    'Connection: Keep-Alive'
+		    ));
+
+			curl_setopt($ch, CURLOPT_POST, 0);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			$response = curl_exec($ch);
+			curl_close($ch);
+			$user = json_decode($response,true);
+			if (count($user)>0)
+				$resultArray['data']['timestamp'] = $user['timestamp'];
 		}
 		catch (Exception $e) {
 			$resultArray[$idolName]['data'] = false;
