1780c432dee14c66807495c5afd69460ae4473ca
Eldad Corem <eldad@Eldads-MacBook-Pro.local>
2018-06-28 21:22:29 +0300

    quick-fix in insert idol

--- insertIdol.php
+++ insertIdol.php
@@ -27,6 +27,7 @@
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			$response = curl_exec($ch);
 			curl_close($ch);
+			$resultArray['data'] = $response;
 		}
 		catch (Exception $e) {
 			$resultArray['result'] = 1;
